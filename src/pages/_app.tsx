e6fd8093c19b9c4339561a53c65a61f6c6a3acc7
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-05-22 23:54:42 -0300

    setup redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import '../common/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import 'common/styles/globals.css';
+import { store } from 'common/store/store';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
+  <Provider store={store}>
     <Head>
       <title>Severino</title>
       <meta name="description" content="Severino" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Component {...pageProps} />
-  </>
+  </Provider>
 );
 
 export default MyApp;
