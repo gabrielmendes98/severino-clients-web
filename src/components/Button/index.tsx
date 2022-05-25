a8f3b9022a2e094561f47c4e430448e3b19acb08
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-05-24 22:59:32 -0300

    fix console warnings and errors

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,14 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef, Ref } from 'react';
 import MuiButton, { ButtonProps } from '@mui/material/Button';
 
 interface Props extends ButtonProps {
   children: ReactNode;
 }
 
-const Button = ({ children, ...props }: Props) => (
-  <MuiButton {...props}>{children}</MuiButton>
+const Button = ({ children, ...props }: Props, ref: Ref<any>) => (
+  <MuiButton {...props} ref={ref}>
+    {children}
+  </MuiButton>
 );
 
-export default Button;
+export default forwardRef(Button);
