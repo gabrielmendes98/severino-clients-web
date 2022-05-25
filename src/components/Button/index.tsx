4df55478fe94145382561302b782d8b08ffc9233
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-05-24 23:08:27 -0300

    button text transform

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,13 @@
 import { ReactNode, forwardRef, Ref } from 'react';
 import MuiButton, { ButtonProps } from '@mui/material/Button';
+import { sx } from './styles';
 
 interface Props extends ButtonProps {
   children: ReactNode;
 }
 
 const Button = ({ children, ...props }: Props, ref: Ref<any>) => (
-  <MuiButton {...props} ref={ref}>
+  <MuiButton {...props} ref={ref} sx={sx}>
     {children}
   </MuiButton>
 );
