b65e4e514e86de78ed070841e3098f52120a849d
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-06-26 00:46:56 -0300

    fix main container responsivity

--- src/components/Layouts/styles.ts
+++ src/components/Layouts/styles.ts
@@ -2,6 +2,7 @@ import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
 
 export const Main = styled(Box)(({ theme }) => ({
+  width: '100%',
   maxWidth: '1440px',
   margin: '0 auto',
   paddingLeft: '20px',
