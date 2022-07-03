7b7cb3719f856c96bcb3132c84b532db12064eeb
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-07-03 19:30:59 -0300

    fix auth route loader

--- src/common/hocs/withAuthentication.tsx
+++ src/common/hocs/withAuthentication.tsx
@@ -4,6 +4,8 @@ import { ComponentType, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { selectUserIsSigned } from 'common/slices/user';
 import { useSelector } from 'common/store/hooks';
+import Loader from 'components/Loader';
+import Box from 'components/Box';
 
 const withAuthentication =
   <T extends object>(Component: ComponentType<T>) =>
@@ -21,7 +23,18 @@ const withAuthentication =
     }, [isSigned, router]);
 
     if (!clientIsSigned) {
-      return <div>loading...</div>;
+      return (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Loader />
+        </Box>
+      );
     }
 
     return <Component {...(props as T)} />;
