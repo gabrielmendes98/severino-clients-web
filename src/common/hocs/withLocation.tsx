59c9fe0f5e8c96362889a58feeb1d50e9586348b
gabrielmendes98 <gabrielmssantiago@gmail.com>
2022-07-07 16:27:23 -0300

    fix refresh without strict mode

--- src/common/hocs/withLocation.tsx
+++ src/common/hocs/withLocation.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/display-name */
 import { ComponentType, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { useSelector } from 'common/store/hooks';
 import { selectHasLocation } from 'common/slices/location';
 import { isOnServer } from 'common/utils/ssr';
@@ -9,6 +10,7 @@ import Modal from 'components/Modal';
 const withLocation =
   <T extends object>(Component: ComponentType<T>) =>
   (props: T) => {
+    const router = useRouter();
     const hasLocation = useSelector(selectHasLocation);
     const [clientHasLocation, setClientHasLocation] = useState(true);
     const [open, setOpen] = useState(true);
@@ -20,7 +22,12 @@ const withLocation =
     if (!clientHasLocation && !isOnServer()) {
       return (
         <Modal open={open}>
-          <Location closeModal={() => setOpen(false)} />
+          <Location
+            closeModal={() => {
+              setOpen(false);
+              router.reload();
+            }}
+          />
         </Modal>
       );
     }
