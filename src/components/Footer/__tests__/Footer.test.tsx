52bf421396bc31420297c4ef79ebe1cf54a45ff3
Gabriel Mendes de Souza Santiago <gabrielmssantiago@gmail.com>
2022-07-08 12:01:15 -0300

    add explanation to footer tests

--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -1,6 +1,8 @@
 import { render } from 'test-utils';
 import Footer from '../index';
 
+// no more tests were added, because the component has no practical functionality for now
+
 it('should match snapshot', () => {
   const { container } = render(<Footer />);
   expect(container).toMatchSnapshot();
